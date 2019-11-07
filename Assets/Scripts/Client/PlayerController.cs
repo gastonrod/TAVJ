0f95f86b25c4c7fe343721008d1a6648866d9169
foviedoITBA <foviedo@itba.edu.ar>
2019-11-07 13:22:42 -0300

    Move PlayerController to Client folder

---
+++ Assets/Scripts/Client/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using Streams;
+using UnityEngine;
+
+namespace Client
+{
+    public class PlayerController
+    {
+        private IStream<IPEndPoint> _stream;
+    
+        public void SetStream(IStream<IPEndPoint> stream)
+        {
+            _stream = stream;
+        }
+    
+        public void Update()
+        {
+            if (_stream == null) return;
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                Debug.Log("PlayerController: Sending down direction");
+                _stream.SendMessage(Protocols.MovementProtocol.Serialize(Protocols.MovementProtocol.Direction.Down));
+            }
+            else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                Debug.Log("PlayerController: Sending up direction");
+                _stream.SendMessage(Protocols.MovementProtocol.Serialize(Protocols.MovementProtocol.Direction.Up));
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                Debug.Log("PlayerController: Sending left direction");
+                _stream.SendMessage(Protocols.MovementProtocol.Serialize(Protocols.MovementProtocol.Direction.Left));
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                Debug.Log("PlayerController: Sending right direction");
+                _stream.SendMessage(Protocols.MovementProtocol.Serialize(Protocols.MovementProtocol.Direction.Right));
+            }
+        }
+    }
+}
